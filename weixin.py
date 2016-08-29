afa23c38ab534ba618c84c4541f05916a5d4d144
winsonyin <winsonyin123@gmail.com>
2016-08-29 21:38:32 +0800

    Use AsyncHTTPClient

--- weixin.py
+++ weixin.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
-
-from settings import db
-
+import tornado.gen
+import tornado.httpclient
 import tornado.options
 from tornado.options import options, define
 
 import xml.etree.ElementTree as ET
 
-from lxml import etree
-
-import requests
 import re
 import json
 
@@ -23,7 +19,6 @@ import hashlib
 
 import os.path
 
-import urllib
 import sys
 
 default_encoding = 'utf-8'
@@ -32,7 +27,7 @@ if sys.getdefaultencoding() != default_encoding:
     sys.setdefaultencoding(default_encoding)
 
 
-define('port', default=80, type=int)
+define('port', default=8000, type=int)
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -57,14 +52,9 @@ class IndexHandler(BaseHandler):
         if hashcode == signature:
             return self.write(echostr)
 
-    def tryFind(self, html):
-        try:
-            city = re.findall('<city>(.*?)</city>', html)[0].encode('utf-8')
-            return True
-        except Exception, e:
-            return False
-        
 
+    @tornado.web.asynchronous
+    @tornado.gen.engine
     def post(self, *args, **kwargs):
         xml_str = self.request.body
         xml = ET.fromstring(xml_str)
@@ -73,10 +63,38 @@ class IndexHandler(BaseHandler):
         createTime = xml.find('CreateTime').text
         msgType = xml.find('MsgType').text
 
+        if msgType == 'text':
+            content = xml.find('Content').text
+            msgId = xml.find('MsgId').text
 
-        if msgType != 'text':
-            text = '1、回复城市名称，可以查询该城市当日天气情况' + '\n' + '如：武汉' + '\n' + ' ' + '\n' + '2、回复笑话，可以收到最新笑话' + '\n' + '如：笑话'
-            text = text.encode('utf-8')
+            if u'笑话' in content:
+                url = "http://www.qiushibaike.com/text/page/%d/?s=4903921" % randint(
+                    1, 35)
+                client = tornado.httpclient.AsyncHTTPClient()
+                response = yield tornado.gen.Task(client.fetch, url)
+
+                content = response.body.encode('utf-8').replace('\n', ' ').replace('\r', ' ').replace(
+                ' ', '').replace('<br/>糗', 'flag').replace('<br/><br/>', 'start')
+
+                contentlist = re.findall('flag.*?start(.*?)start', content)
+
+                index = randint(0, len(contentlist)-1)
+                content = contentlist[index-1].replace('<br/>', '\n')
+                content = content.encode('utf-8')
+
+
+            else:
+                key = 'b4875784bc9d43eaa07239cecfc7f661'
+                api = 'http://www.tuling123.com/openapi/api?key=' + key + '&info='
+
+                url = api + content.encode('utf-8')
+                client = tornado.httpclient.AsyncHTTPClient()
+                response = yield tornado.gen.Task(client.fetch, url)
+                dic_json = json.loads(response.body)
+                content = dic_json['text']
+                print content
+                content = content.encode('utf-8')
+
             reply = '''
             <xml>
             <ToUserName><![CDATA[%s]]></ToUserName>
@@ -85,113 +103,45 @@ class IndexHandler(BaseHandler):
             <MsgType><![CDATA[%s]]></MsgType>
             <Content><![CDATA[%s]]></Content>
             </xml>
-            ''' % (
-                fromUserName,
-                toUserName,
-                createTime,
-                'text',
-                text,
-            )
-            return self.write(reply)
-
-        content = xml.find('Content').text
-        msgId = xml.find('MsgId').text
-
-        url = 'http://wthrcdn.etouch.cn/WeatherApi?city=%s' % content
-        html = requests.get(url).content
-
-    	if u'笑话' in content:
-            url = "http://www.qiushibaike.com/text/page/%d/?s=4903921" % randint(1, 35)
-    	    r = requests.get(url)
-            tree = etree.HTML(r.text)
-    	    contentlist = tree.xpath('//div[@class="article block untagged mb15"]')
-            jokes = []
-
-    	    for i in contentlist:
-                content = i.xpath('div[@class="content"]/text()')
-	        contentstring = ''.join(content)
-    	        contentstring = contentstring.strip('\n')
-    	        jokes.append(contentstring)
-        
-            index = randint(0, len(jokes))
-    	    content = jokes[index-1]
-            content = content.encode('utf-8')
-        
-        elif self.tryFind(html):
-            city = re.findall('<city>(.*?)</city>', html)[0].encode('utf-8')
-            wendu = re.findall('<wendu>(.*?)</wendu>', html)[0].encode('utf-8')
-            shidu = re.findall('<shidu>(.*?)</shidu>', html)[0].encode('utf-8')
-
-            try:
-                aqi = re.findall('<aqi>(.*?)</aqi>', html)[0].encode('utf-8')
-                pm25 = re.findall('<pm25>(.*?)</pm25>', html)[0].encode('utf-8')
-                suggest = re.findall('<suggest>(.*?)</suggest>', html)[0].encode('utf-8')
-                quality = re.findall('<quality>(.*?)</quality>', html)[0].encode('utf-8')
-                o3 = re.findall('<o3>(.*?)</o3>', html)[0].encode('utf-8')
-                co = re.findall('<co>(.*?)</co>', html)[0].encode('utf-8')
-                pm10 = re.findall('<pm10>(.*?)</pm10>', html)[0].encode('utf-8')
-                so2 = re.findall('<so2>(.*?)</so2>', html)[0].encode('utf-8')
-                no2 = re.findall('<no2>(.*?)</no2>', html)[0].encode('utf-8')
-                flag = 1
-            except Exception, e:
-                flag = 0
-
-
-            date = re.findall('<date>(.*?)</date>', html)
-            high = re.findall('<high>(.*?)</high>', html)
-            low = re.findall('<low>(.*?)</low>', html)
-
-            Type = re.findall('<type>(.*?)</type>', html)
-            fengxiang = re.findall('<fengxiang>(.*?)</fengxiang>', html)
-            fengli = re.findall('<fengli>(.*?)</fengli>', html)
-
-            content_1 = "城市：" + city + "\n" + date[0].encode('utf-8') + "\n" + \
-             "温度：" + wendu + "℃" + "\n" + high[0].encode('utf-8') + "\n" + \
-             low[0].encode('utf-8') + "\n" + " " + "\n" + \
-             "白天：" + Type[0].encode('utf-8') + " " + fengxiang[1].encode('utf-8') + " " + \
-             fengli[1].encode('utf-8') + "\n" + \
-             "晚上：" + Type[1].encode('utf-8') + " " + fengxiang[2].encode('utf-8') + " " + \
-             fengli[2].encode('utf-8') + " " + "\n"
-
-            if flag == 1:
-                content_2 = "\n" + "空气质量指数：" + aqi + "\n" + "湿度：" + shidu + "\n" + "pm2.5：" + \
-                pm25 + "\n" + "空气质量：" + quality + "\n" + "臭氧：" + \
-                o3 + "\n" + "一氧化碳：" + co + "\n" + \
-                "颗粒物(PM10)：" + pm10 + "\n" + "二氧化硫：" + so2 + "\n" + "二氧化氮：" + no2 + "\n" + "建议：" + suggest
-
-                content = content_1 + content_2
-            else:
-                content = content_1
-
-	else:
-    	    key = '8b005db5f57556fb96dfd98fbccfab84'  
-            api = 'http://www.tuling123.com/openapi/api?key=' + key + '&info='  
-
-            request = api + content.encode('utf-8')
-            page = urllib.urlopen(request)  
-            html = page.read()
-            dic_json = json.loads(html)  
-            content = '小银: '.decode('utf-8') + dic_json['text']
-            content = content.encode('utf-8')
-
-    	reply = '''
-    	<xml>
-    	<ToUserName><![CDATA[%s]]></ToUserName>
-    	<FromUserName><![CDATA[%s]]></FromUserName>
-    	<CreateTime>%s</CreateTime>
-    	<MsgType><![CDATA[%s]]></MsgType>
-    	<Content><![CDATA[%s]]></Content>
-    	</xml>
-    	''' % (fromUserName, toUserName, createTime, msgType, content)
-    	return self.write(reply)
+            ''' % (fromUserName, toUserName, createTime, msgType, content)
+            self.write(reply)
+            self.finish()
 
 
 class HelloHandler(BaseHandler):
+
+    # @tornado.web.asynchronous
+    # @tornado.gen.engine
     def get(self, *args, **kwargs):
-        return self.write("Python say: Hello winson")
+        # url = 'http://wthrcdn.etouch.cn/WeatherApi?city=%s' % '武汉'
+
+        # sync
+        # http_request = tornado.httpclient.HTTPRequest(url, method='GET')
+        # synClient = tornado.httpclient.HTTPClient()
+        # response = synClient.fetch(http_request)
+
+        # async
+        # client = tornado.httpclient.AsyncHTTPClient()
+        # response = yield tornado.gen.Task(client.fetch, url)
+        # print response.body
+
+        # xml = ET.fromstring(response.body)
+
+        # print url
+        # fengli = xml.find('fengli').text
+        # print fengli 
+
+        # request = urllib.urlopen(url)
+        # htmlCode = request.read()
+        # print htmlCode
+
+
+        return self.write("Python say: Hello Winson")
+        # self.finish()
 
 
 class Application(tornado.web.Application):
+
     def __init__(self):
         handlers = [
             (r'/wechat', IndexHandler),
